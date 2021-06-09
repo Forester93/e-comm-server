f29d4df24da065ae39ef86182dd936fa8264e3e4
Marco <markforester93@gmail.com>
2021-06-10 00:15:59 +1000

    minor changes category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,9 +8,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
   try {
     const categories = await Category.findAll({
-      include: [
-        { model: Product, attributes: ["product_name", "price", "stock"] },
-      ],
+      include: [{ model: Product }],
     });
     res.status(200).json(categories);
   } catch (err) {
@@ -23,9 +21,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Products
   try {
     const categories = await Category.findByPk(req.params.id, {
-      include: [
-        { model: Product, attributes: ["product_name", "price", "stock"] },
-      ],
+      include: [{ model: Product }],
     });
 
     if (!categories) {
@@ -75,7 +71,7 @@ router.delete("/:id", async (req, res) => {
       res.status(404).json({ message: "Record not found!" });
       return;
     }
-    res.status(200).send("Record deleted!");
+    res.status(200).json(categories);
   } catch (err) {
     res.status(500).json(err);
   }
